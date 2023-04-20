6486c88735cede7c502b00f7fc99d4d8303991b9
1 <1>
2023-04-20 17:00:52 +0800

    update

--- deep_sort/deep_sort/nn_matching.py
+++ deep_sort/deep_sort/nn_matching.py
@@ -41,12 +41,12 @@ def _cosine_distance(a, b, data_is_normalized=False):
         An LxM matrix of L samples of dimensionality M.
     data_is_normalized : Optional[bool]
         If True, assumes rows in a and b are unit length vectors.
-        Otherwise, a and b are explicitly normalized to lenght 1.
+        Otherwise, a and b are explicitly normalized to length 1.
         如果为真，则假定a和b中的行是单位长度的向量。否则，a和b将显式归一化为长度为1
     Returns
     -------
     ndarray
-        Returns a matrix of size len(a), len(b) such that eleement (i, j)
+        Returns a matrix of size len(a), len(b) such that element (i, j)
         contains the squared distance between `a[i]` and `b[j]`.
 
     """
