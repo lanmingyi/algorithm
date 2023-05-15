4ceace6f259829158fab8f2c5bbf28fdf1cfe3d5
1 <1>
2023-05-15 17:01:41 +0800

    update

---
+++ tms/IJCAI20/ALNS/README.md
@@ -0,0 +1,2 @@
+
+Algorithm based on Ropke and Pisinger paper ALNS solution for PDPTW, included in this source
---
+++ tms/YmmRecommenderSystems/user_loss_prediction/utils.py
@@ -0,0 +1,13 @@
+# @Description : 数据处理通用方法
+
+
+def load_origin_data(file_name):
+    active = {}
+    with open(file_name, 'r') as fread:
+        line = fread.readline().strip()
+        while line:
+            active_days = line.split(",")
+            active[active_days[0]] = active_days[1:]
+            line = fread.readline().strip()
+    return active
+
