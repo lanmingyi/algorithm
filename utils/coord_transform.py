76bd683db81e225f92c2a530e3615c43fe6eb00d
1 <1>
2023-05-15 11:57:41 +0800

    update

---
+++ utils/coord_transform.py
@@ -0,0 +1,203 @@
+"""
+简单介绍一下几种常见的坐标系：
+
+WGS84坐标系：即地球坐标系（World Geodetic System），国际上通用的坐标系。设备包含的GPS芯片或者北斗芯片获取的经纬度一般都是为WGS84地理坐标系，目前谷歌地图采用的是WGS84坐标系（中国范围除外）。
+
+GCJ02坐标系：GCJ-02是由中国国家测绘局（G表示Guojia国家，C表示Cehui测绘，J表示Ju局）制订的地理信息系统的坐标系统。由WGS84坐标系经加密后的坐标系。谷歌中国采用的GCJ02地理坐标系。也称：火星坐标系。
+
+BD09坐标系：即百度坐标系，GCJ02坐标系经加密后的坐标系。
+
+Web 墨卡托投影坐标系：也称web墨卡托，是如今主流的Web地图使用的坐标系，如国外的 Google Maps，OpenStreetMap，Bing Map，ArcGIS 和 Heremaps 等，
+国内的百度地图、高德地图、腾讯地图和天地图等也是基于Web墨卡托，与地理坐标系不同，投影坐标系的单位是m（由于国内政策的原因，国内地图会有加密要求，
+一般有两种情况，一种是在 Web墨卡托的基础上经过国家标准加密的国标02坐标系，熟称“火星坐标系”；另一种是在国标的02坐标系下进一步进行加密，如百度地图的BD09坐标系）。
+墨卡托投影的“等角”特性，保证了对象的形状的不变行，正方形的物体投影后不会变为长方形。“等角”也保证了方向和相互位置的正确性，因此在航海和航空中常常应用，而Google们在计算人们查询地物的方向时不会出错。
+
+"""
+import json
+import urllib
+import math
+import time
+
+x_pi = 3.14159265358979324 * 3000.0 / 180.0
+# pi = math.pi  # π
+pi = 3.1415926535897932384626  # π
+a = 6378245.0  # 长半轴
+es = 0.00669342162296594323  # 偏心率平方。扁率
+
+
+class Geocoding:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def geocode(self, address):
+        """
+        利用高德geocoding服务解析地址获取位置坐标
+        :param address:需要解析的地址
+        :return:
+        """
+        geocoding = {'s': 'rsv3',
+                     'key': self.api_key,
+                     'city': '全国',
+                     'address': address}
+        geocoding = urllib.urlencode(geocoding)
+        ret = urllib.urlopen("%s?%s" % ("http://restapi.amap.com/v3/geocode/geo", geocoding))
+
+        if ret.getcode() == 200:
+            res = ret.read()
+            json_obj = json.loads(res)
+            if json_obj['status'] == '1' and int(json_obj['count']) >= 1:
+                geocodes = json_obj['geocodes'][0]
+                lng = float(geocodes.get('location').split(',')[0])
+                lat = float(geocodes.get('location').split(',')[1])
+                return [lng, lat]
+            else:
+                return None
+        else:
+            return None
+
+
+def gcj02_to_bd09(lng, lat):
+    """
+    火星坐标系(GCJ-02)转百度坐标系(BD-09)
+    谷歌、高德——>百度
+    :param lng:火星坐标经度
+    :param lat:火星坐标纬度
+    :return:
+    """
+    z = math.sqrt(lng * lng + lat * lat) + 0.00002 * math.sin(lat * x_pi)
+    theta = math.atan2(lat, lng) + 0.000003 * math.cos(lng * x_pi)
+    bd_lng = z * math.cos(theta) + 0.0065
+    bd_lat = z * math.sin(theta) + 0.006
+    return [bd_lng, bd_lat]
+
+
+def bd09_to_gcj02(bd_lng, bd_lat):
+    """
+    百度坐标系(BD-09)转火星坐标系(GCJ-02)
+    百度——>谷歌、高德
+    :param bd_lat:百度坐标纬度
+    :param bd_lng:百度坐标经度
+    :return:转换后的坐标列表形式
+    """
+    x = bd_lng - 0.0065
+    y = bd_lat - 0.006
+    z = math.sqrt(x * x + y * y) - 0.00002 * math.sin(y * x_pi)
+    theta = math.atan2(y, x) - 0.000003 * math.cos(x * x_pi)
+    gcj_lng = z * math.cos(theta)
+    gcj_lat = z * math.sin(theta)
+    return [gcj_lng, gcj_lat]
+
+
+def wgs84_to_gcj02(lng, lat):
+    """
+    WGS84转GCJ02(火星坐标系)
+    :param lng:WGS84坐标系的经度
+    :param lat:WGS84坐标系的纬度
+    :return:
+    """
+    if out_of_china(lng, lat):  # 判断是否在国内
+        return lng, lat
+    dlat = _transformlat(lng - 105.0, lat - 35.0)
+    dlng = _transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - es * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - es)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    gcj_lat = lat + dlat
+    gcj_lng = lng + dlng
+    return [gcj_lng, gcj_lat]
+
+
+def gcj02_to_wgs84(gcj_lng, gcj_lat):
+    """
+    GCJ02(火星坐标系)转GPS84
+    :param lng:火星坐标系的经度
+    :param lat:火星坐标系纬度
+    :return:
+    """
+    if out_of_china(gcj_lng, gcj_lat):
+        return gcj_lng, gcj_lat
+    dlat = _transformlat(gcj_lng - 105.0, gcj_lat - 35.0)
+    dlng = _transformlng(gcj_lng - 105.0, gcj_lat - 35.0)
+    radlat = gcj_lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - es * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - es)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = gcj_lat + dlat
+    mglng = gcj_lng + dlng
+    return [gcj_lng * 2 - mglng, gcj_lat * 2 - mglat]
+
+
+def bd09_to_wgs84(bd_lng, bd_lat):
+    lng, lat = bd09_to_gcj02(bd_lng, bd_lat)
+    return gcj02_to_wgs84(lng, lat)
+
+
+def wgs84_to_bd09(lng, lat):
+    lng, lat = wgs84_to_gcj02(lng, lat)
+    return gcj02_to_bd09(lng, lat)
+
+
+def _transformlat(lng, lat):
+    ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+          0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lat * pi) + 40.0 *
+            math.sin(lat / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * math.sin(lat / 12.0 * pi) + 320 *
+            math.sin(lat * pi / 30.0)) * 2.0 / 3.0
+    return ret
+
+
+def _transformlng(lng, lat):
+    ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+          0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lng * pi) + 40.0 *
+            math.sin(lng / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 *
+            math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
+    return ret
+
+
+def out_of_china(lng, lat):
+    """
+    判断是否在国内，不在国内不做偏移
+    :param lng:
+    :param lat:
+    :return:
+    """
+    # return not (lng > 73.66 and lng < 135.05 and lat > 3.86 and lat < 53.55)
+    return not (73.66 < lng < 135.05 and 3.86 < lat < 53.55)
+
+
+def timestamp_to_time(timestamp):
+    # 时间戳转时间
+    time_local = time.localtime(int(timestamp))
+    dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+    return str(dt)
+
+
+def time_to_timestamp(time_str):
+    # 时间装时间戳
+    timeArray = time.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+    timestamp = time.mktime(timeArray)
+    return timestamp
+
+
+if __name__ == '__main__':
+    lng = 128.543
+    lat = 37.065
+    result1 = gcj02_to_bd09(lng, lat)
+    result2 = bd09_to_gcj02(lng, lat)
+    result3 = wgs84_to_gcj02(lng, lat)
+    result4 = gcj02_to_wgs84(lng, lat)
+    result5 = bd09_to_wgs84(lng, lat)
+    result6 = wgs84_to_bd09(lng, lat)
+    print(result1)
